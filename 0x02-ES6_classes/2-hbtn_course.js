cd15dab3ca2a1f7029cd4372a42610c42bdc86bb
PeteJames21 <peetjames21@gmail.com>
2024-03-07 23:37:32 +0300

    Implement a class that modesls a course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+// Implement a class that models a course
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  _name;
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('"name" must be a string');
+    }
+    this._name = value;
+  }
+
+  _length;
+  get _length() {
+    return this._length;
+  }
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('"length" must be a number');
+    }
+    this._length = value;
+  }
+
+  _students;
+  get students() {
+    return this.students;
+  }
+  set students(value) {
+    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
+      throw new TypeError('"students" must be an array of strings');
+    }
+    this._students = value;
+  }
+}
