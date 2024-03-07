01a5437c986d164b9d568dcda4c7889ee235fd21
PeteJames21 <peetjames21@gmail.com>
2024-03-07 23:18:33 +0300

    Add documentation

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,10 @@
 import ClassRoom from "./0-classroom.js";
 
+/**
+ * Create 3 Classroom instances
+ *
+ * @returns - an array of three Classroom instances
+ */
 export default function initializeRooms () {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]
 }
