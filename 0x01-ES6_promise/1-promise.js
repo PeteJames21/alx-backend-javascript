7d670087fd2b64f15ca450dc7aef34cc899a3e3d
PeteJames21 <peetjames21@gmail.com>
2024-03-08 22:09:41 +0300

    Fix esline issues

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,9 +4,9 @@
  * @returns a promise
  */
 export default function getFullResponseFromAPI(success) {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
-      resolve({status: 200, body: 'success'});
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
