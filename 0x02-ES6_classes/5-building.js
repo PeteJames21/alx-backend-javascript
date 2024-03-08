48fca1facb8bb99f2a5229df5785c5feca31bdf1
PeteJames21 <peetjames21@gmail.com>
2024-03-08 11:15:28 +0300

    Fix: change conditional

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -6,7 +6,7 @@
  */
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building || this.evacuationWarningMessage === undefined) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
 
