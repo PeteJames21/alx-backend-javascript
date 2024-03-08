9d271f14ee7c1de764cbe1f63d9c2ad3092aaba0
PeteJames21 <peetjames21@gmail.com>
2024-03-08 23:56:42 +0300

    Return result of division

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+// Demonstrate how to throw an error
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
