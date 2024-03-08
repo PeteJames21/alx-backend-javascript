52cc6c20e5a243f13eb0e2cb9e74ccfd441c700e
PeteJames21 <peetjames21@gmail.com>
2024-03-08 11:44:25 +0300

    Add docstring

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -2,6 +2,11 @@
  * Models an airport
  */
 export default class airport {
+  /**
+   *
+   * @param {string} name
+   * @param {string} code
+   */
   constructor(name, code) {
     this._name = name;
     this._code = code;
