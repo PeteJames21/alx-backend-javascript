277e0917566185f0d5b2f6d66aaf1f83ed43a668
PeteJames21 <peetjames21@gmail.com>
2024-03-08 11:11:58 +0300

    Create and abstract class for modelling a building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+/**
+ * An abstract class for modelling a building.
+ *
+ * This class cannot be instantiated and must implement the method
+ * evacuationWarningMessage().
+ */
+export default class Building {
+  constructor(sqft) {
+    if (new.target === Building || this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
